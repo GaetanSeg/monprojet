e7ab9c61ce9575e7511508da8e3a9b1590ef91a6
happymusic <supra3946@gmail.com>
2018-02-22 18:53:41 +0100

    php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php phpinfo(); echo "helo"; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>php test</title>
+  </head>
+  <body>
+    <h3>PHP TEST</h3>
+    <p> voici un paragraphe qui contient du php -> <?php echo "hello world"; ?></p>
+
+  </body>
+</html>
