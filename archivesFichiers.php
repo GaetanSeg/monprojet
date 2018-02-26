499e32b3671f5255e1e33d87a7988688a528afe9
happymusic <supra3946@gmail.com>
2018-02-26 17:46:01 +0100

    change archives

--- archivesFichiers.php
+++ archivesFichiers.php
@@ -1,18 +1,63 @@
 
-        <p>Nous sommes le <?php echo date('d/m/Y h:m:s');?> <br></p>
-		
-        <?php
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>php test</title>
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+  </head>
 
-        $adresse="../../CPAS/Celulle Encodage_partage/";
-        $dossier=Opendir($adresse);
-		echo "$adresse"
-        while ($Fichier = readdir($dossier))
-        {
-             if ($Fichier != "." && $Fichier != "..")//permet d'enlever les points
-             {
-                  echo '<li><a href='.$adresse.$Fichier.' target="_blank">'.$Fichier.'</a></li><br>';
-             }
-        }
-        closedir($dossier);
-		
-        ?>
+  <body>
+
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">PHP TEST</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+        </div>
+      </nav>
+    </header>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+      <br><br><br><br>
+      <p>Nous sommes le <?php echo date('d/m/Y h:m:s');?> <br></p>
+
+      <?php
+
+            $adresse="../../Documents/";
+            $dossier=Opendir($adresse);
+
+    		echo "$adresse";
+            while ($Fichier = readdir($dossier))
+            {
+                 if ($Fichier != "." && $Fichier != "..")//permet d'enlever les points
+                 {
+                      echo '<li><a href='.$adresse.$Fichier.' target="_blank">'.$Fichier.'</a></li><br>';
+                 }
+            }
+            closedir($dossier);
+
+      ?>
+
+
+    </main>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+</body>
+</html>
