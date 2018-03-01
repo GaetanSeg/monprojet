468eb09f9267490a4a491cd7684ff9d3b49a056f
happymusic <supra3946@gmail.com>
2018-03-01 20:38:54 +0100

    test

--- monprojet/include/footer.php
+++ monprojet/include/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
         <div class="container">
-          <span class="text-muted">2018-Seggio Gaetan</span>
+          <span class="text-muted"><b>2018-Seggio Gaetan</b></span>
         </div>
 </footer>
