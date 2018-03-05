9b664501f906ef09cfb0cd186a5d1eacfa19039f
happymusic <supra3946@gmail.com>
2018-03-05 22:40:46 +0100

    php

--- monprojet/login.php
+++ monprojet/login.php
@@ -1,7 +1,7 @@
 <?php $title = 'Index'; ?>
 <?php ob_start(); ?>
 <?php
-$login= NULL;
+
     try
     {
       //ouverture d'un porte dans une db
@@ -11,6 +11,8 @@ $login= NULL;
     {
             die('Erreur : '. $e->getMessage());
     }
+    $login= "";
+    $password="";
 
     $myReq = $bdd->prepare('SELECT * FROM user WHERE login = :login');//  les ":login" sert a dire que indiquer que login serra remplacer lors de l'execution de la requete
     $myReq->execute(array('login' => $_POST['login']));//remplacement de login par le login du form
