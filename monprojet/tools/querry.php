3bcb4d6df47a2b2342904d0e0632d50a7d278f01
happymusic <supra3946@gmail.com>
2018-03-12 18:10:14 +0100

    test

--- monprojet/tools/querry.php
+++ monprojet/tools/querry.php
@@ -1,12 +1,13 @@
 <?php
-    require 'connectDb.php';
+require 'connectDb.php';
 function getQuery(){
+
   $bdd = getDataBase();
 
   $myReq = $bdd->prepare('SELECT * FROM user WHERE login = :login');//  les ":login" sert a dire que indiquer que login serra remplacer lors de l'execution de la requete
   $myReq->execute(array('login' => $_POST['login']));//remplacement de login par le login du form
   $resultat = $myReq->fetch();
-  
+
   return $resultat;
 }
 
