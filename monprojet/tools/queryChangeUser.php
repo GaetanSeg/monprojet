4da2848b9b71421dedf0f4c3ad8022dc64de0c7a
happymusic <supra3946@gmail.com>
2018-03-17 11:53:13 +0100

    php

--- monprojet/tools/queryChangeUser.php
+++ monprojet/tools/queryChangeUser.php
@@ -2,18 +2,7 @@
       require 'connectDb.php';
 
         $bdd = getDataBase();
-
-        //$myReq = $bdd->prepare('SELECT * FROM user WHERE login = :login');//  les ":login" sert a dire que indiquer que login serra remplacer lors de l'execution de la requete
-
-        //$resultat = $myReq->fetch();
         $reponse = $bdd->query('SELECT * FROM USER');
-        //reponse est un objet PDOStatement (voir doc)
-      /*while ($donnees = $reponse->fetch())
-        {
-            echo "<b>Login USER</b> : ".$donnees['login']."<br><a href=\"editUser.php\" class=\"btn btn-info\" role=\"button\">Editer</a>";
-            echo '<br>';
-            echo "<hr>";
-        }*/
 
         while ($donnees = $reponse->fetch())
         {
