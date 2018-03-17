e6d65d3caff621a7e026ec343145535b05197cf4
happymusic <supra3946@gmail.com>
2018-03-17 11:33:03 +0100

    change

--- espaceMembre/login.php
+++ espaceMembre/login.php
@@ -1,12 +1,12 @@
 <?php
 
-if (!empty($_POST) && !empty($_POST['username']) && !empty($_POST['Password'])) 
+if (!empty($_POST) && !empty($_POST['username']) && !empty($_POST['Password']))
 {
 	require_once 'inc/db.php';
 	require_once 'inc/functions.php';
 
 	$req = $pdo->prepare( 'SELECT * FROM users WHERE username = :username OR email = :username');
-	$reqConf= $pdo->prepare( 'SELECT * FROM users WHERE confirmed_at IS not null');  
+	$reqConf= $pdo->prepare( 'SELECT * FROM users WHERE confirmed_at IS not null');
 	$req->execute(['username' => $_POST['username']]);
 	$reqConf->execute(['username' => $_POST['username']]);
 	$user = $req->fetch();
@@ -34,16 +34,16 @@ if (!empty($_POST) && !empty($_POST['username']) && !empty($_POST['Password']))
 
 
 	<form action="" method="post" >
-		
+
 		<div class="form-group">
-			
+
 			<label for="">Pseudo ou Email*</label>
 			<input type="text" name="username" class="form-control" required />
 
 		</div>
-		
+
 		<div class="form-group">
-			
+
 			<label for="">Mot de passe* </label>
 			<input type="Password" name="Password" class="form-control" required/>
 
