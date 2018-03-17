23405e46793300ab161d3262c458bb066469421d
happymusic <supra3946@gmail.com>
2018-03-17 12:03:31 +0100

    FINAL

--- monprojet/tools/queryChangeUser.php
+++ monprojet/tools/queryChangeUser.php
@@ -8,10 +8,10 @@
         {
 
           echo '<form action="editUser.php" method="GET">
-                  <b>ID</b> : <input type="text" name="id"  value="'.$donnees['id'].'"/><br>
-                  <b>Login</b> : <input type="text" name="login" value="'.$donnees['login'].'"/><br>
-                  <b>Password</b> : <input type="password" name="password" value="'.$donnees['password'].'"/><br>
-                  <input type="submit"/>
+                  <b>ID</b> : <input type="text" class="form-control" name="id"  value="'.$donnees['id'].'"/><br>
+                  <b>Login</b> : <input type="text" class="form-control" name="login" value="'.$donnees['login'].'"/><br>
+                  <b>Password</b> : <input type="password" class="form-control" name="password" value="'.$donnees['password'].'"/><br>
+                  <input type="submit" class="btn btn-primary"/>
                   <hr>
                 </form>';
 
